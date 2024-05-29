fe0d2b82f2a58f857be9dc64e13d6267d368560d
xenshre <151603584+shrey426@users.noreply.github.com>
2024-05-29 12:16:03 +0530

    Update GLTFLoader.js

--- GLTFLoader.js
+++ GLTFLoader.js
@@ -2,7 +2,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const loader = new GLTFLoader();
 const loadedData = await loader.loadAsync(
-  "https://cdn.jsdelivr.net/gh/XenReality-Technologies-Private-Limited/imageTrackingModels@main/scene.gltf"
+  "https://cdn.jsdelivr.net/gh/XenReality-Technologies-Private-Limited/imageTrackingModels@main/assets/scene.gltf"
 );
 
 loader.parseAsync(loadedData,'',function(gltf){
@@ -10,5 +10,3 @@ loader.parseAsync(loadedData,'',function(gltf){
 },function(){
 	console.log('model load error')
 });
-
-
