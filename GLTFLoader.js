31aa30e611370984b818f08c1198e67a9837da27
xenshre <151603584+shrey426@users.noreply.github.com>
2024-05-29 17:36:59 +0530

    Update GLTFLoader.js

--- GLTFLoader.js
+++ GLTFLoader.js
@@ -1,12 +1,25 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from 'three';
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const loader = new GLTFLoader();
-const loadedData = await loader.loadAsync(
-  "https://cdn.jsdelivr.net/gh/XenReality-Technologies-Private-Limited/imageTrackingModels@main/assets/scene.gltf"
-);
+const url = 'https://cdn.jsdelivr.net/gh/XenReality-Technologies-Private-Limited/imageTrackingModels@main/assets/scene.gltf"'; 
 
-loader.parseAsync(loadedData,'',function(gltf){
-	scene.add(gltf.scene);
-},function(){
-	console.log('model load error')
+loader.load(url, function(gltf) {
+    scene.add(gltf.scene);
+    camera.position.z = 5;
+    animate();
+}, undefined, function(error) {
+    console.error(error);
 });
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
